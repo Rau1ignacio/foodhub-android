d13c1ea22d375999db19f97d10302db6666619a6
Rau1ignacio <rau.bustamante@duocuc.cl>
2025-10-23 02:22:09 -0300

    Creacion de repos

--- app/src/main/java/com/example/foodhub/ui/viewmodels/ViewModelFactory.kt
+++ app/src/main/java/com/example/foodhub/ui/viewmodels/ViewModelFactory.kt
@@ -1,2 +1,24 @@
 package com.example.foodhub.ui.viewmodels
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.foodhubtest.data.repository.FoodRepository
+
+/**
+ * Fábrica simple. Sabe cómo crear ViewModels que
+ * SÓLO necesitan el 'FoodRepository'.
+ */
+class ViewModelFactory(private val repo: FoodRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return when {
+            // Si piden un AdminVM, lo creo con 'repo'
+            modelClass.isAssignableFrom(AdminVM::class.java) -> AdminVM(repo) as T
+
+            // Si piden un HomeVM, lo creo con 'repo'
+            modelClass.isAssignableFrom(HomeVM::class.java) -> HomeVM(repo) as T
+
+            // Si piden uno que no conozco, lanza error
+            else -> throw IllegalArgumentException("Unknown ViewModel class provided to ViewModelFactory: ${modelClass.name}")
+        }
+    }
+}
--- app/src/main/java/com/example/foodhub/ui/viewmodels/ViewModelFactoryWithSession.kt
+++ app/src/main/java/com/example/foodhub/ui/viewmodels/ViewModelFactoryWithSession.kt
@@ -1,2 +1,31 @@
 package com.example.foodhub.ui.viewmodels
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.foodhubtest.data.repository.FoodRepository
+
+/**
+ * Fábrica especializada que sabe cómo inyectar el 'repo' Y el 'sessionVM'
+ * a los ViewModels que los requieran.
+ */
+class ViewModelFactoryWithSession(
+    private val repo: FoodRepository,
+    private val sessionVM: SessionVM // La dependencia compartida
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return when {
+            // Si piden un AuthVM, lo creo con 'repo' y 'sessionVM'
+            modelClass.isAssignableFrom(AuthVM::class.java) -> AuthVM(repo, sessionVM) as T
+
+            // Si piden un OrderHistoryVM, lo creo con 'repo' y 'sessionVM'
+            modelClass.isAssignableFrom(OrderHistoryVM::class.java) -> OrderHistoryVM(repo, sessionVM) as T
+
+            // Si piden un CartVM, lo creo con 'repo' y 'sessionVM'
+            modelClass.isAssignableFrom(CartVM::class.java) -> CartVM(repo, sessionVM) as T
+
+            // Si piden otro VM (que no necesita SessionVM), uso la otra fábrica
+            else -> ViewModelFactory(repo).create(modelClass)
+        }
+    }
+}
